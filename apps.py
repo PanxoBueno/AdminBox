9af84802cb6357e1aba18cb1501ea72c275d2f06
PanxoBueno <151773348+PanxoBueno@users.noreply.github.com>
2025-05-06 21:35:51 -0400

    Delete apps.py

--- apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class GymAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gym_app'
-    
-    def ready(self):
-        import gym_app.signals 
