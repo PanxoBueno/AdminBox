24fc3fceb9cbc2eea9f8f17cc079cd05b84f68ca
PanxoBueno <151773348+PanxoBueno@users.noreply.github.com>
2025-05-06 21:32:21 -0400

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Atleta, Entrenador, Biblioteca, Usuario
-# Register your models here. aca se registran los modelos para ser CRUD en perfil admin
-
-admin.site.register(Atleta)
-admin.site.register(Entrenador)
-admin.site.register(Biblioteca)
-admin.site.register(Usuario)
