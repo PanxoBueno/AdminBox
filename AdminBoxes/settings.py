95e03c988bcdd00decc0d448c22cc4dd237392d2
PanxoBueno <151773348+PanxoBueno@users.noreply.github.com>
2025-05-12 14:20:41 -0400

    Actualización v3
    Se agrega funcionalidad de email y calendarios

--- AdminBoxes/settings.py
+++ AdminBoxes/settings.py
@@ -146,3 +146,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Configuración de Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # O tu servidor SMTP
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'tuadmibox@gmail.com'  # Cambiar por tu email
+EMAIL_HOST_PASSWORD = 'ogeayukuvpjhcpbb'  # Cambiar por tu contraseña o app password
+DEFAULT_FROM_EMAIL = 'tuadmibox@gmail.com'  # Mismo que EMAIL_HOST_USER
